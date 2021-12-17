286aefb1243d4939452b43743ad1a5a8d8b20f00
N.Mukai <mukai@chirashiya.co.jp>
2021-12-17 16:10:00 +0900

    バグ修正

--- maxx_tcd043-child/functions.php
+++ maxx_tcd043-child/functions.php
@@ -4,5 +4,6 @@ function enqueue_child_styles()
 {
   // enqueue style
   wp_enqueue_style('parent', get_template_directory_uri() . '/style.css');
+  wp_enqueue_style('child', get_stylesheet_directory_uri() . '/style.css');
 }
 add_action('wp_enqueue_scripts', 'enqueue_child_styles');
--- oops_tcd048-child/functions.php
+++ oops_tcd048-child/functions.php
@@ -3,6 +3,6 @@
 function childtheme_enqueue_scripts()
 {
   wp_enqueue_style('parent', get_template_directory_uri() . '/style.css', array('oops-slick', 'oops-slick-theme', 'oops-lightbox'));
-  wp_enqueue_style('child', get_template_directory_uri() . '/style.css', array('parent'));
+  wp_enqueue_style('child', get_stylesheet_directory_uri() . '/style.css', array('parent'));
 }
 add_action('wp_enqueue_scripts', 'childtheme_enqueue_scripts');
