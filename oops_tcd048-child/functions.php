b727ea5cf878a40127b16e61fbf562b2e7ec2996
N.Mukai <mukai@chirashiya.co.jp>
2021-12-23 16:37:13 +0900

    バグを修正

--- oops_tcd048-child/functions.php
+++ oops_tcd048-child/functions.php
@@ -1,8 +1,14 @@
 <?php
 /* enqueue script for parent theme stylesheeet */
-function childtheme_enqueue_scripts()
+function childtheme_read_styles()
 {
-  wp_enqueue_style('parent', get_template_directory_uri() . '/style.css', array('oops-slick', 'oops-slick-theme', 'oops-lightbox'));
+
+  // enqueue style
+  if (is_front_page()) {
+    wp_enqueue_style('parent', get_template_directory_uri() . '/style.css', array('oops-slick', 'oops-slick-theme', 'oops-lightbox'));
+  } else {
+    wp_enqueue_style('parent', get_template_directory_uri() . '/style.css', array());
+  }
   wp_enqueue_style('child', get_stylesheet_directory_uri() . '/style.css', array('parent'));
 }
-add_action('wp_enqueue_scripts', 'childtheme_enqueue_scripts');
+add_action('wp_enqueue_scripts', 'childtheme_read_styles');
